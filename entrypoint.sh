c6419a7ca27514f5be58bb16a7438083bc81d6b4
Marko Jovanovic <mjovanovic@control4.com>
2022-02-23 20:02:01 +0100

    added version update to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 cd $1
 
-python3 /app/dp3/driverpackager.py -v ./ $3 $2
+if [ -z "$4" ]; then
+    python3 /app/dp3/driverpackager.py -v ./ $3 $2 --driver-version $4
+        else
+    python3 /app/dp3/driverpackager.py -v ./ $3 $2
+fi
