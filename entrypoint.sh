94e45c3a83bc327ea1c17a74c7402aa91e97b453
Marko Jovanovic <mjovanovic@control4.com>
2022-03-02 13:54:25 +0100

    not null fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ cd $1
 
 cmd = python3 /app/dp3/driverpackager.py -v ./ $3 $2
 
-if [ -z "$4"]; then
+if [ ! -z "$4"]; then
     cmd+=" --driver-version $4"
 fi
 
-if [ -z "$5"]; then
+if [ ! -z "$5"]; then
     cmd+=" --update-modified $5"
 fi
 
